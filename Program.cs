f9804b53c4b98213d72777097ded0c6d16ab81f5
Cameron <77825507+CameronWDavis@users.noreply.github.com>
2023-10-27 00:11:35 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,142 +1,182 @@
-// See https://aka.ms/new-console-template for more information
-using System; 
-namespace tictacproject 
+using System;
+
+namespace TicTacToeProject
 {
-    //constructor for point object
     class Point
     {
-        //methods and getters
-        public int x{get; set; }
-        public int y {get; set;}
+        // Properties to store the X and Y coordinates and the value ('X' or 'O') of a point on the board
+        public int X { get; set; }
+        public int Y { get; set; }
+        public string Value { get; set; }
 
-        public String value{get; set;}
-
-
-        //constructor 
-        public Point(int x, int y, String value){
-            this.x = x;  
-            this.y = y; 
-            this.value = value;
+        // Constructor to initialize a point with coordinates and value
+        public Point(int x, int y, string value)
+        {
+            X = x;
+            Y = y;
+            Value = value;
         }
     }
-    class program{
 
+    class Program
+    {
         static void Main(string[] args)
         {
-            //local strings 
-            String playerInput, userInput, computerPlayer,x, gameChoice;
-            x = "X";
-            //array for answers 
-            String[] fillOutBoard = new String[9]; 
-            Console.WriteLine("Welcome to the tik tac toe simulator!"); 
-            Console.WriteLine("Would you like to be X or O?"); 
-              playerInput = Console.ReadLine(); 
-             Console.WriteLine("\n"); 
+            // Initialize variables to store player and computer inputs
+            string playerInput, computerPlayer;
+            string[] board = new string[9];
+            // Initialize the board with empty spaces
+            for (int i = 0; i < 9; i++) board[i] = " ";
+            // Welcome message
+            Console.WriteLine("Welcome to the Tic Tac Toe simulator!");
+            // Prompt the player to choose X or O
+            Console.WriteLine("Would you like to be X or O?");
+            playerInput = Console.ReadLine().ToUpper();
+            // Set the computer's piece based on the player's choice
+            computerPlayer = playerInput == "X" ? "O" : "X";
+            Console.WriteLine("\nYou will be playing as " + playerInput + " and I as " + computerPlayer);
 
-             //verify user is playing with correct pieaces 
-              userInput = checkInput(playerInput); 
-              //operator to msee if computer will be X or O 
-             computerPlayer = userInput.Equals(x, StringComparison.OrdinalIgnoreCase) ? computerPlayer = "O" : computerPlayer = "X"; 
-             Console.WriteLine("You will be playing as " + userInput + " and I as " + computerPlayer); 
-             //printBoard(); 
-
-             Console.WriteLine("Lets start!"); 
-            //this is used for testing
-             Point[] pointArray = new Point[8];
-             Point test = playerTurn(userInput);
-             checkBoard(pointArray,test);
-
-
-             Random rnd = new Random();
-             int randomInt = rnd.Next(1, 3);
-            if (randomInt == 1) {
-                Console.WriteLine("Computer goes first.");
-                int randomX = rnd.Next(1,3);
-                int randomY = rnd.Next(1,3);
-                Point move = new Point(randomX,randomY,computerPlayer);
-            } else {
-                Console.WriteLine("Player goes first.");
-               Point firstMove =  playerTurn(userInput);
-               
-            }
-            
-            
-
-
-        }
-
-
-
-
- 
-
-    static Point playerTurn(String userInput){
-         while (true) {
-        Console.Write("Enter a point in the format of x,y (between 1 and 3): ");
-        string gameChoice = Console.ReadLine();
-        string[] coordinates = gameChoice.Split(',');
-        int x = int.Parse(coordinates[0]);
-        int y = int.Parse(coordinates[1]);
-
-        if (x >= 1 && x <= 3 && y >= 1 && y <= 3) {
-            Point point = new Point(x,y,userInput);
-            return point;
-        }
-
-        Console.WriteLine("Invalid input. Please enter values between 1 and 3.");
-    }
-    }
-
-    //function to add user move to array and print the required piece
-       static void checkBoard(Point[] points,Point moveMade){
-
-       int index = (moveMade.x - 1) * 3 + (moveMade.y - 1);//function to map index
-       points[index] = moveMade;
-       Console.WriteLine(moveMade.value);
-       
-           
-        }
-
-        
-
-
-    //Function to verify user input 
-        static String checkInput(String argument){
-            //local variables
-            String test = argument; 
-            String x = "X"; 
-            String o = "O"; 
-            //seeing if user input is null 
-            if(String.IsNullOrEmpty(test)){
-                while(String.IsNullOrEmpty(test)){
-                Console.WriteLine("Enter a Value to play the game!");
-                test = Console.ReadLine(); 
+            // Variable to keep track of whose turn it is
+            bool isPlayerTurn = true;
+            // Main game loop
+            while (true)
+            {
+                // Print the current state of the board
+                PrintBoard(board);
+                if (isPlayerTurn)
+                {
+                    // Get the player's move
+                    Point playerMove = PlayerTurn(playerInput);
+                    // Convert the player's move to an index in the board array
+                    int index = (playerMove.X - 1) * 3 + (playerMove.Y - 1);
+                    // Check if the move is valid
+                    if (board[index] == " ")
+                    {
+                        // Make the move
+                        board[index] = playerInput;
+                        // Check if the player has won
+                        if (CheckWinner(board, playerInput))
+                        {
+                            PrintBoard(board);
+                            Console.WriteLine("Congratulations! You win!");
+                            break;
+                        }
+                        // Switch to the computer's turn
+                        isPlayerTurn = false;
+                    }
+                    else
+                    {
+                        // If the move is invalid, prompt the player to try again
+                        Console.WriteLine("Invalid move. Try again.");
+                    }
+                }
+                else
+                {
+                    // Computer's turn
+                    Console.WriteLine("Computer's turn.");
+                    Point computerMove = ComputerTurn(computerPlayer, board);
+                    int index = (computerMove.X - 1) * 3 + (computerMove.Y - 1);
+                    // Make the computer's move
+                    board[index] = computerPlayer;
+                    // Check if the computer has won
+                    if (CheckWinner(board, computerPlayer))
+                    {
+                        PrintBoard(board);
+                        Console.WriteLine("Computer wins!");
+                        break;
+                    }
+                    // Switch to the player's turn
+                    isPlayerTurn = true;
+                }
+                // Check if the board is full (resulting in a draw)
+                if (IsBoardFull(board))
+                {
+                    PrintBoard(board);
+                    Console.WriteLine("It's a draw!");
+                    break;
                 }
             }
-            //testing that user inpout is correct length
-            if(test.Length > 1){
-                while(test.Length > 1){
-                    Console.WriteLine("Player cant play with " + test); 
-                    Console.WriteLine("Please try again"); 
-                    test = Console.ReadLine(); 
-                } 
-            }
+        }
 
-            if(test.Equals(x, StringComparison.OrdinalIgnoreCase)){
-                return test; 
+        // Method to get the player's move
+        static Point PlayerTurn(string userInput)
+        {
+            while (true)
+            {
+                Console.Write("Enter a point in the format of x,y (between 1 and 3): ");
+                string gameChoice = Console.ReadLine();
+                string[] coordinates = gameChoice.Split(',');
+                int x, y;
+                // Validate the input and return the player's move if it's valid
+                if (coordinates.Length == 2 && int.TryParse(coordinates[0], out x) && int.TryParse(coordinates[1], out y) && x >= 1 && x <= 3 && y >= 1 && y <= 3)
+                {
+                    return new Point(x, y, userInput);
+                }
+                Console.WriteLine("Invalid input. Please enter values between 1 and 3.");
             }
-            else if(test.Equals(o, StringComparison.OrdinalIgnoreCase)){
-                return test; 
+        }
+
+        // Method to get the computer's move
+        static Point ComputerTurn(string computerInput, string[] board)
+        {
+            Random rnd = new Random();
+            while (true)
+            {
+                int randomX = rnd.Next(1, 4);
+                int randomY = rnd.Next(1, 4);
+                int index = (randomX - 1) * 3 + (randomY - 1);
+                // Ensure the computer's move is valid
+                if (board[index] == " ")
+                {
+                    return new Point(randomX, randomY, computerInput);
+                }
             }
-            else{
-                    Console.WriteLine("Tic Tac Toe is played with X and O"); 
-                    test = Console.ReadLine(); 
-                //recursion to validate 
-                test = checkInput(test); 
-            
-            }           
-            //return the real value to the user
-            return test; 
+        }
+
+        // Method to check if a player has won
+        static bool CheckWinner(string[] board, string player)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                // Check rows and columns
+                if ((board[i * 3] == player && board[i * 3 + 1] == player && board[i * 3 + 2] == player) ||
+                    (board[i] == player && board[i + 3] == player && board[i + 6] == player))
+                {
+                    return true;
+                }
+            }
+            // Check diagonals
+            if ((board[0] == player && board[4] == player && board[8] == player) ||
+                (board[2] == player && board[4] == player && board[6] == player))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        // Method to check if the board is full
+        static bool IsBoardFull(string[] board)
+        {
+            foreach (var spot in board)
+            {
+                if (spot == " ")
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        // Method to print the current state of the board
+        static void PrintBoard(string[] board)
+        {
+            Console.WriteLine(" 1 2 3");
+            Console.WriteLine("1 " + board[0] + "|" + board[1] + "|" + board[2]);
+            Console.WriteLine(" -----");
+            Console.WriteLine("2 " + board[3] + "|" + board[4] + "|" + board[5]);
+            Console.WriteLine(" -----");
+            Console.WriteLine("3 " + board[6] + "|" + board[7] + "|" + board[8]);
         }
     }
 }
+
